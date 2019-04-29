6c59f5e11de31568a64232a46a5844d12e690506
Agust Rafnsson <agust@urbit.com>
2019-04-29 16:40:57 +0200

    tried and failed to implement everything through core data. Need to rethink.

---
+++ pooch/pooch/Breed.swift
@@ -0,0 +1,13 @@
+//
+//  Breed.swift
+//  pooch
+//
+//  Created by Agust Rafnsson on 2019-04-29.
+//  Copyright © 2019 Electrolux. All rights reserved.
+//
+
+import UIKit
+
+class Breed: NSManagedObject {
+
+}
---
+++ pooch/pooch/Image.swift
@@ -0,0 +1,13 @@
+//
+//  Image.swift
+//  pooch
+//
+//  Created by Agust Rafnsson on 2019-04-29.
+//  Copyright © 2019 Electrolux. All rights reserved.
+//
+
+import UIKit
+
+class Image: NSManagedObject {
+
+}
